286c99b27ed870aa671a34a95d1e2483883e49eb
Guy Segal <guysegal.il@gmail.com>
2022-01-11 20:21:34 +0200

    delete all services

--- delete_services.sh
+++ delete_services.sh
@@ -1,6 +1,4 @@
-set -e
 kubectl delete -n default -f ./deployments/default/mongo.yaml
 kubectl delete -n default -f ./deployments/default/kafka.yaml
 kubectl delete -n job-management -f ./deployments/job-management
 kubectl delete -n fulfillment -f ./deployments/fulfillment
-exit 0
