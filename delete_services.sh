5e42bd787394f920b85a98a1aa0a14ce88d32f52
Guy Segal <guysegal.il@gmail.com>
2022-01-13 19:04:45 +0200

    .

--- delete_services.sh
+++
@@ -1,4 +0,0 @@
-kubectl delete -n default -f ./deployments/default/mongo.yaml
-kubectl delete -n default -f ./deployments/default/kafka.yaml
-kubectl delete -n job-management -f ./deployments/job-management
-kubectl delete -n fulfillment -f ./deployments/fulfillment
