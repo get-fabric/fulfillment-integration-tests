b181e188061285d69d8c203cba6a5f0c51d6cb00
Guy Segal <guysegal.il@gmail.com>
2022-03-08 19:38:19 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,11 +1,9 @@
 test_runner_service_name=$1
 test_runner_endpoint=$2
 
-echo "waiting before port forward"
-sleep 20
 kubectl -n fulfillment port-forward svc/$test_runner_service_name 3478:80 &
 echo "waiting after port forward"
-sleep 20
+sleep 10
 
 echo "calling test runner $test_runner_endpoint endpoint..."
 status_code=$(curl --max-time 120 --verbose -LI http://localhost:3478/$test_runner_endpoint -o /dev/null -w '%{http_code}\n' -s)
