3b8c7e8fd2425ce38bbabc4863fd29a1ef762a5f
Guy Segal <guysegal.il@gmail.com>
2022-03-08 19:15:12 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,9 @@
 test_runner_service_name=$1
 test_runner_endpoint=$2
 
-kubectl -n fulfillment port-forward svc/$test_runner_service_name 3478:80 &
 sleep 10
+
+kubectl -n fulfillment port-forward svc/$test_runner_service_name 3478:80 &
 
 echo "calling test runner $test_runner_endpoint endpoint..."
 status_code=$(curl --max-time 120 --verbose -LI http://localhost:3478/$test_runner_endpoint -o /dev/null -w '%{http_code}\n' -s)
