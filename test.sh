b40d6a90715af8b944139508881afef0d03b1865
Guy Segal <guysegal.il@gmail.com>
2022-03-31 09:19:13 +0300

    .

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 test_runner_service_name=$1
 test_runner_endpoint=$2
+kubeconigPath=${3:-~/.kube/config}
 
 kubectl -n fulfillment port-forward svc/$test_runner_service_name 3478:80 &
 echo "waiting after port forward"
