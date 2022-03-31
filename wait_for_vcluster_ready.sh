4a64a0f55dae8dca6b17ab45888bfc11055f3b1f
Guy Segal <guysegal.il@gmail.com>
2022-03-31 08:29:27 +0300

    .

--- wait_for_vcluster_ready.sh
+++ wait_for_vcluster_ready.sh
@@ -6,7 +6,7 @@ get_ready_vcluster_pod_count() {
     echo $((`kubectl get pods -A | grep vcluster-1-0 | grep $vclusterName | grep Running | grep 2/2 | wc -l`))
 }
 
-sleepSeconds=${1:-5}
+sleepSeconds=${2:-5}
 
 while true
 do
