fa8dd3f19e10b5caa6ab7bde9367ad33ab9a620d
Guy Segal <guysegal.il@gmail.com>
2022-04-03 08:14:53 +0300

    vcluster (#10)

--- test.sh
+++ test.sh
@@ -1,18 +1,18 @@
 test_runner_service_name=$1
 test_runner_endpoint=$2
+kubeconigPath=${3:-~/.kube/config}
 
-kubectl -n fulfillment port-forward svc/$test_runner_service_name 3478:80 &
-echo "waiting after port forward"
+kubectl --kubeconfig $kubeconigPath -n fulfillment port-forward svc/$test_runner_service_name 3478:80 &
 sleep 10
 
 echo "calling test runner $test_runner_endpoint endpoint..."
-status_code=$(curl --max-time 120 --verbose -LI http://localhost:3478/$test_runner_endpoint -o /dev/null -w '%{http_code}\n' -s)
-echo "test runner result: $status_code"
+IFS=$'\n' read -d "" body status_code  < <(curl --max-time 120 -s -w "\n%{http_code}\n" "http://localhost:3478/sanity")
+echo "statusCode $status_code"
+echo "traceId: $body"
 
 if [[ "$status_code" == "200" ]]
-    then echo "success"
+then
     exit 0
 else
-    echo "failure"
     exit 1
 fi
