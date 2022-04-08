cf447af129acaaffdd59d0bd38366d6607d4715b
Guy Segal <guysegal.il@gmail.com>
2022-04-08 14:52:06 +0300

    deploy all services every test run (#11)

---
+++ wait_for_pods.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+get_total_pods_count () {
+    echo $((`kubectl --kubeconfig $kubeconigPath get pods --all-namespaces | grep -v "kube-system" | grep -E $expectedStatus | wc -l` - 1))
+}
+
+get_not_ready_pods_count () {
+    echo $((`kubectl --kubeconfig $kubeconigPath get pods --all-namespaces | grep -v "kube-system" | grep -E $expectedStatus  | grep 0/ | wc -l`))
+}
+
+expectedStatus=${1}
+sleepSeconds=${2:-5}
+kubeconigPath=${3:-~/.kube/config}
+
+total=0
+ready=-1
+until [ $total -eq $ready ]
+do
+    sleep $((sleepSeconds))
+
+    total=$(get_total_pods_count)
+    notReady=$(get_not_ready_pods_count)
+    ready=$(( total - notReady ))
+
+    echo $ready/$total pods ready
+done
