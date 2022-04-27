5e1b85bb74c454335c5245ea74315a46a1a5d014
Guy Segal <guysegal.il@gmail.com>
2022-04-27 10:25:33 +0300

    wait for pod ready

--- wait_for_pod_ready.sh
+++ wait_for_pod_ready.sh
@@ -9,7 +9,6 @@ until [[ -z "$result" ]]
 do
     sleep 1
     pod_running=`kubectl --kubeconfig $kubeconigPath get pod $pod_name -n $namespace | grep Running | wc -l`
-    echo $pod_running
     if [ $pod_running == "1" ]
     then
         result=`kubectl --kubeconfig $kubeconigPath get pod $pod_name -n $namespace -o=jsonpath='{$.status.containerStatuses[*].ready}' | grep false`
