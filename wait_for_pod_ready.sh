3514b350c6aab3b725b971568d1481cd365b01ea
Guy Segal <guysegal.il@gmail.com>
2022-04-27 09:50:52 +0300

    wait for pod ready

--- wait_for_pod_ready.sh
+++ wait_for_pod_ready.sh
@@ -2,11 +2,19 @@
 
 pod_name=${1}
 namespace=${2}
+kubeconigPath=${3:-~/.kube/config}
 
 result="not_ready"
 until [[ -z "$result" ]]
 do
-sleep 5
-result=`kubectl get pod $pod_name -n $namespace -o=jsonpath='{$.status.containerStatuses[*].ready}' | grep false`
+    sleep 1
+    pod_running=`kubectl --kubeconfig $kubeconigPath get pod $pod_name -n $namespace | grep Running | wc -l`
+    echo $pod_running
+    if [ $pod_running == "1" ]
+    then
+        result=`kubectl --kubeconfig $kubeconigPath get pod $pod_name -n $namespace -o=jsonpath='{$.status.containerStatuses[*].ready}' | grep false`
+    fi
 done
+echo "vcluster ready"
+
 
