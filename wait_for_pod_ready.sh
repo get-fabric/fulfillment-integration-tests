845ebced751c3267fa188cd549e5de476753cb7a
Guy Segal <guysegal.il@gmail.com>
2022-06-16 12:26:17 +0300

    upgrade to vcluster v0.10.0

---
+++ wait_for_pod_ready.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+pod_name=${1}
+namespace=${2}
+kubeconigPath=${3:-~/.kube/config}
+
+result="not_ready"
+until [[ -z "$result" ]]
+do
+    sleep 1
+    pod_running=`kubectl --kubeconfig $kubeconigPath get pod $pod_name -n $namespace | grep Running | wc -l`
+    if [ $pod_running == "1" ]
+    then
+        result=`kubectl --kubeconfig $kubeconigPath get pod $pod_name -n $namespace -o=jsonpath='{$.status.containerStatuses[*].ready}' | grep false`
+    fi
+done
+echo "vcluster ready"
